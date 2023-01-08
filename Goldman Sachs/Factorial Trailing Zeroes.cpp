b0400ac664a63b72da936a892c05ebe39f598806
Ayush kumar <jhaaayushmarch@gmail.com>
2023-01-08 21:11:49 +0530

    Add files via upload

--- Goldman Sachs/Factorial Trailing Zeroes.cpp
+++ Goldman Sachs/Factorial Trailing Zeroes.cpp
@@ -1,8 +1,14 @@
 class Solution {
 public:
     int trailingZeroes(int n) {
-        if(n < 5) return 0;
-        else
-            return n/5 + trailingZeroes(n/5);
+    	 int ans = 0;
+        for(int i = 5; i <= n; i*=5) 
+        {
+            ans = ans + (n/i);
+        }
+        return ans;
+//        if(n < 5) return 0;
+//        else
+//            return n/5 + trailingZeroes(n/5);
     }
 };
