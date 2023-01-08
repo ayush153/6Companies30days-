5aad0f49e3bce2161cf7dfc11a3c809d113adcfb
Ayush kumar <jhaaayushmarch@gmail.com>
2023-01-08 20:50:54 +0530

    Add files via upload

--- Goldman Sachs/Number of Boomerangs.cpp
+++ Goldman Sachs/Number of Boomerangs.cpp
@@ -1,22 +1,12 @@
 class Solution {
 public:
-     int helper(int n)
-     {
-        return n*(n-1);
-    }
     int numberOfBoomerangs(vector<vector<int>>& points) {
-         int n = points.size();
         int ans = 0;
-        for(int i = 0; i < n; i++)
+        for (auto &p : points) 
         {
-            unordered_map<int,int>mp;
-            for(int j = 0; j < n; j++)
-            {
-                int dist = pow(points[i][0]-points[j][0],2) + pow(points[i][1]-points[j]                    [1],2);
-                mp[dist]++;
-            }
-            for(auto it:mp) 
-                ans += helper(it.second); 
+            unordered_map<double, int> mp(points.size());
+            for (auto &q : points)
+                ans += 2 * mp[hypot(p[0] - q[0], p[1] - q[1])]++;
         }
         return ans;
     }
