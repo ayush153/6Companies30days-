5caceb5a2e16ebbf8960beb8627c00b02981435b
Ayush kumar <jhaaayushmarch@gmail.com>
2023-01-24 13:09:27 +0530

    Add files via upload

--- Google/Number of Closed Islands.cpp
+++ Google/Number of Closed Islands.cpp
@@ -1,55 +1,29 @@
 class Solution {
 public:
-     bool bfs(int row, int col, vector<vector<int>>&vis, vector<vector<int>>&grid, int del_row[], int del_col[]) {
-        queue<pair<int,int>>q;
-        q.push({row,col}); 
-        vis[row][col] = 1;
-        bool isClosed = true;
-        while(q.empty() == NULL)
-        {
-            int r = q.front().first;
-            int c = q.front().second;
-            q.pop();
-            for(int i = 0; i < 4; i++)
-            {
-                int nrow = r + del_row[i];
-                int ncol = c + del_col[i];
-                
-                if(nrow >=0 && nrow < grid.size() && ncol >= 0 && ncol < grid[0].size() && grid[nrow][ncol] == 0 && vis[nrow][ncol] == 0)
-                {
-                    if(nrow == 0 || nrow == grid.size() - 1 || ncol == 0 || ncol == grid[0].size() - 1)
-                    {
-                         isClosed = false;
-                    }
-                    else
-                    {
-                        q.push({nrow, ncol});
-                        vis[nrow][ncol] = 1;
-                    }
+     int res = 0;
+        for (int i = 0; i < grid.size(); i++){
+            for (int j = 0; j < grid[0].size(); j++){
+                if (grid[i][j] == 0){
+                    res += dfs(grid, i, j) ? 1 : 0;
                 }
             }
         }
-        return isClosed;
+        return res;
     }
-    
-    int closedIsland(vector<vector<int>>& grid) {
-        int n = grid.size();
-        int m = grid[0].size();
-        int islands = 0;
-        int del_row[4] = {-1,0,+1,0};
-        int del_col[4] = {0,1,0,-1};
-        vector<vector<int>>vis(n, vector<int>(m,0));
-        for(int i = 1; i < n-1; i++)
-        {
-            for(int j = 1; j < m-1; j++)
-            {
-                if(vis[i][j] == 0 && grid[i][j] == 0 &&  bfs(i,j,vis,grid,del_row,del_col))
-                {
-                        islands++;
-                }
-            }
+    bool dfs(vector<vector<int>>& g, int i, int j){
+        if (i < 0 || j < 0 || i >= g.size() || j >= g[0].size()){
+            return false;
         }
-        return islands;
+        if (g[i][j] == 1){
+            return true;
+        }
+        g[i][j] = 1;
+       
+        bool d1 = dfs(g, i+1, j);
+        bool d2 = dfs(g, i, j+1);
+        bool d3 = dfs(g, i-1, j);
+        bool d4 = dfs(g, i, j-1);
+        return d1 && d2 && d3 && d4;
     }
     
 };
