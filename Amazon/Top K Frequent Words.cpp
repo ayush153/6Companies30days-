bba4e0d6b66f0c494343d484fbc5dcd60ac39d7c
Ayush kumar <jhaaayushmarch@gmail.com>
2023-01-27 22:01:58 +0530

    Add files via upload

---
+++ Amazon/Top K Frequent Words.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    vector<string> topKFrequent(vector<string>& words, int k) {
+        unordered_map<string, int>frequency;
+
+        for(auto word: words)
+        {
+            frequency[word]++;
+        }
+
+        vector<string>ans;
+        while(k--)
+        {
+            int maxy = 0;
+            string str;
+            for(auto [key, val]: frequency)
+            {
+                if(val > maxy)
+                {
+                    maxy = val;
+                    str = key;
+                }
+                else if(val == maxy)
+                {
+                    if(str > key)
+                    {
+                        str = key;
+                    }
+                }
+            }
+            ans.push_back(str);
+            frequency[str] = -1;
+            cout<<str<<' '<<frequency[str];
+        }
+
+        return ans;
+    }
+};
